0e9d3934f0ed3c8c40ef4ef88c1e249f513b0af7
Lucimara <lucimarasouzah@gmail.com>
2022-12-13 16:31:36 -0300

    Backend

---
+++ Backend/ProjetoTurmaFiap/Controllers/TurmaController.cs
@@ -0,0 +1,100 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Projeto.Data.Dto;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ProjetoTurmaFiap.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurmaController : ControllerBase
+    {
+        private readonly Projeto.Data.Interfaces.ITurmaRepositorio _turmaRepositorio;
+
+        public TurmaController(
+            Projeto.Data.Interfaces.ITurmaRepositorio turmaRepositorio)
+        {
+            _turmaRepositorio = turmaRepositorio;
+        }
+
+        [HttpGet]
+        [Route("/ListarTodas")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Projeto.Data.Dto.TurmaDto>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult ListarTodas()
+        {
+            try
+            {
+                return Ok(_turmaRepositorio.ListarTodas());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("/PorId/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Projeto.Data.Dto.TurmaDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult PorId(int id)
+        {
+            if (id < 1)
+                return NoContent();
+
+            try
+            {
+                TurmaDto resultado = _turmaRepositorio.PorId(id);
+
+                if (resultado == null)
+                    return NoContent();
+
+                return Ok(resultado);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("/Cadastrar")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Cadastrar(AlunoCadastrarDto cadastrarDto)
+        {
+            if (cadastrarDto == null || String.IsNullOrEmpty(cadastrarDto.Nome))
+                return NoContent();
+
+            return BadRequest();
+        }
+
+        [HttpPatch]
+        [Route("/Atualizar")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Atualizar(AlunoCadastrarDto cadastrarDto)
+        {
+            if (cadastrarDto == null || cadastrarDto.Id < 1)
+                return NoContent();
+
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("/Excluir")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Excluir(int id)
+        {
+            if (id < 1)
+                return NoContent();
+
+            return BadRequest();
+        }
+
+    }
+}
